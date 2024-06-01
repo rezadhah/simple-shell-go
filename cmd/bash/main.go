d75ad7018c361ea3eb50688c44464af8b61435e5
Reza Adha Hamonangan <rezadhah@protonmail.com>
2024-06-01 14:34:14 +0700

    remove const commands from main.go because it has been move into internal/command package

--- cmd/bash/main.go
+++ cmd/bash/main.go
@@ -6,11 +6,6 @@ import (
 	"github.com/rezadhah/shell/internal/shell"
 )
 
-const (
-	CD_COMMAND   = "cd"
-	EXIT_COMMAND = "exit"
-)
-
 func main() {
 	sh := shell.NewShell()
 	if err := sh.InputMode(); err != nil {
